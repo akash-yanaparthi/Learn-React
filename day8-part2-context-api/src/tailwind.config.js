fc0383eb79a0c6ab56d44e03e6116d5abe42f9b1
akash5265 <vram5265@gmail.com>
2025-02-28 01:26:19 +0530

    done

---
+++ day8-part2-context-api/src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class', // This is where you configure dark mode
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
