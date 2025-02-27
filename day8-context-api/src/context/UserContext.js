b3d92b0f43b07fefc49342f3cf45a3c9cf263692
akash5265 <vram5265@gmail.com>
2025-02-28 00:19:20 +0530

    done with the project

---
+++ day8-context-api/src/context/UserContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const UserContext =  React.createContext()
+
+export default UserContext;   //all context are providers at the end, because the work they do is to povide the data
